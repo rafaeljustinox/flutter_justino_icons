34c8b83f59954332224636b7ea4786486e13f2d4
Rafael Justino <rafael01justino@gmail.com>
2021-06-27 16:54:12 -0300

    See CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.0.2
+
+* Improved the example
+* Renamed social icons
+
 ## 0.0.1
 
 * First version
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,6 +27,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  dynamic _selected = JustinoIcons.a3d;
+
+  _selectIcon(dynamic icon){
+    setState(() {
+      _selected = icon;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,15 +43,60 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: GridView.extent(
-        padding: EdgeInsets.all(12.0),
-        maxCrossAxisExtent: 72,
-        children: JustinoIcons.allIcons.map((icon){
-          return Icon(
-            icon,
-            size: 38.0,
-          );
-        }).toList()
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Container(
+                width: 200.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                  border: Border.all()
+                ),
+                alignment: Alignment.center,
+                child: Stack(
+                  fit: StackFit.expand,
+                  alignment: Alignment.center,
+                  children: [
+                    Icon(_selected, size: 84.0 ),
+                    Positioned(
+                      bottom: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Wrap(
+                          children: [
+                            Text(
+                              _selected.name,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                  ],
+                )
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.extent(
+              padding: EdgeInsets.all(12.0),
+              maxCrossAxisExtent: 82,
+              children: JustinoIcons.allIcons.map((icon){
+                return InkWell(
+                  onTap: (){
+                    _selectIcon(icon);
+                  },
+                  child: Icon(
+                    icon,
+                    size: 38.0,
+                  ),
+                );
+              }).toList()
+            ),
+          ),
+        ],
       ),
     );
   }
